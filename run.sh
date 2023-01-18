a74005345ed0c4351616bb2a71522c5b2f355df0
KwanghyunOn <kwanghyun.on@gmail.com>
2023-01-18 14:04:56 +0900

    .

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python -m gpustat_web --port 25555 --ssh-port 8022 cup eos gai her iri jup khe lun mar nik ody pos rhe sel ura ven
+python -m gpustat_web --port 25555 --ssh-port 8022 cup dem eos fre gai her iri jup khe lun mar nik ody pos rhe sel ura ven zeu
